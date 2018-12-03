e47cf4c0b8e4c0ac52268c76c7334f8eb4a144d8
Daniil Sokolyuk <d.sokolyuk@rambler-co.ru>
2018-12-03 12:26:12 +0300

    try example with webpack

---
+++ src/ZeroReact.Sample.Webpack.AspNetCore/ClientApp/client.js
@@ -0,0 +1,8 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.React = React;
+window.ReactDOM = ReactDOM;
+
+// Expose components globally so ReactJS.NET can use them
+var Components = require('expose?Components!./components');
---
+++ src/ZeroReact.Sample.Webpack.AspNetCore/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: {
+        server: './ClientApp/server.js',
+        client: './ClientApp/client.js',
+    },
+    output: {
+        filename: './wwwroot/[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    resolveLoader: {
+        moduleExtensions: ['-loader'],
+    },
+};
