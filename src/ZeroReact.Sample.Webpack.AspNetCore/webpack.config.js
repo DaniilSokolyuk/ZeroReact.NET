5b0a9419a955c9d53f46440dd6d84fe5379872c9
Evgeny Bykovskih <redzumi.tao@gmail.com>
2018-12-03 13:11:22 +0300

    Add root files into components folder, remove build files from repo

--- src/ZeroReact.Sample.Webpack.AspNetCore/webpack.config.js
+++ src/ZeroReact.Sample.Webpack.AspNetCore/webpack.config.js
@@ -1,21 +1,24 @@
 module.exports = {
-    entry: {
-        server: './ClientApp/server.js',
-        client: './ClientApp/client.js',
-    },
-    output: {
-        filename: './wwwroot/[name].bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-        ],
-    },
-    resolveLoader: {
-        moduleExtensions: ['-loader'],
-    },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  entry: {
+    server: './ClientApp/server.js',
+    client: './ClientApp/client.js'
+  },
+  output: {
+    filename: './wwwroot/[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolveLoader: {
+    moduleExtensions: ['-loader']
+  }
 };
