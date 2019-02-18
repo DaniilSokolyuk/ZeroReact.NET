9bca26dc6a24e9568ef81ad92bdc038142e983f5
Daniil Sokolyuk <d.sokolyuk@rambler-co.ru>
2019-02-18 19:09:08 +0300

    appveyor

---
+++ build/Build.ps1
@@ -0,0 +1,34 @@
+Paths
+$packFolder = (Get-Item -Path "./nupkg" -Verbose).FullName
+$slnPath = (Get-Item -Path "./" -Verbose).FullName
+$srcPath = Join-Path $slnPath "src"
+
+# List of projects
+$projects = (
+    "JavaScriptEngineSwitcher.ChakraCore",
+    "ZeroReact",
+    "ZeroReact.AspNetCore"
+)
+
+# Rebuild solution
+Set-Location $slnPath
+& dotnet restore
+
+# Copy all nuget packages to the pack folder
+foreach ($project in $projects) {
+    
+    $projectFolder = Join-Path $srcPath $project
+
+    # Create nuget pack
+    Set-Location $projectFolder
+    Remove-Item -Recurse (Join-Path $projectFolder "bin/Release")
+    & dotnet msbuild /t:pack /p:Configuration=Release /p:IncludeSymbols=true /p:SourceLinkCreate=true
+
+    # Copy nuget package
+    $projectPackPath = Join-Path $projectFolder ("/bin/Release/" + $project + ".*.nupkg")
+    Move-Item $projectPackPath $packFolder
+
+}
+
+# Go back to the pack folder
+Set-Location $slnPath
